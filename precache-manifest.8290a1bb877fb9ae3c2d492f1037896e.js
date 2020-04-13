60a1b9afaac6a300ecf8d0af675a9f055371d7c8
James Sudhir <jamessudhir@Sudhir-MacBook-Air.local>
2020-04-13 13:47:26 +0530

    Updates

---
+++ precache-manifest.8290a1bb877fb9ae3c2d492f1037896e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348902ea743bfc06a1abd511a9aef98d",
+    "url": "/FreeThreeGame/index.html"
+  },
+  {
+    "revision": "ee1516d4cda1cbf8c53a",
+    "url": "/FreeThreeGame/static/css/main.9e7e8b34.chunk.css"
+  },
+  {
+    "revision": "d3defcf77cc1bf61380d",
+    "url": "/FreeThreeGame/static/js/2.6e488f24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FreeThreeGame/static/js/2.6e488f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee1516d4cda1cbf8c53a",
+    "url": "/FreeThreeGame/static/js/main.f0a79f8d.chunk.js"
+  },
+  {
+    "revision": "ebe4edaf271f2997652d",
+    "url": "/FreeThreeGame/static/js/runtime-main.45124d62.js"
+  },
+  {
+    "revision": "544aa83b3f6ce39880434016c73fbeb4",
+    "url": "/FreeThreeGame/static/media/bg.544aa83b.png"
+  },
+  {
+    "revision": "c167a0355d04ff269937ba856ab1e4c4",
+    "url": "/FreeThreeGame/static/media/win.c167a035.png"
+  }
+]);
